9a785d2e563ecc37866daa7b7863abe609647f36
jamesjlandry <james.j.landry@outlook.com>
2021-05-07 16:18:06 -0500

    working on the styling. I don't like the material ui cards, updating to vanilla css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 
 import './App.css';
 import { TextField } from '@material-ui/core'
+
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useState } from "react"
 import film from './film.mp4'
 
-
 const useStyles = makeStyles({
-  nomination: {
-    minWidth: 275,
-    maxWidth: 500,
-    margin: '24px'
-  },
-  search: {
-    minWidth: 300,
-    maxWidth: 600,
-    margin: "24px;"
-  },
- 
   title: {
     fontSize: 14,
   },
   pos: {
-    marginBottom: 12,
-  },
-});
+    marginBotom: 12,
+  }
+})
+
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
   
   const [nominations, setNominations] = useState([])
   const [movies, setMovies] = useState()
@@ -69,44 +56,44 @@ function App() {
           <source src={film} type='video/mp4' /> 
       </video>
         <div className="title" ><h1 >The Shoppies</h1></div>
-        <form onSubmit={e => getMovies(e.target.value)}>
+        <form className="search"  onSubmit={e => getMovies(e.target.value)}>
           <TextField label="Title" placeholder="Enter Title" onChange={e =>  getMovies(e)}>
           </TextField>
         </form>
         {nominations.length === 5 
         ? 
-        <Card>
-          <CardContent>
+        <div>
+          <div>
             Congratulations, you have nominated 5 movies!
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       : 
       null
       }
           {(movies)
           ? 
-          <Card className={classes.search}>
-            {movies.map(movie => <Card><CardContent><Typography>{movie.Title}</Typography><Typography>{movie.Year}</Typography>
-          <CardActions>{nominations.includes(movie) ? <Button disabled size="small" onClick={e => addNomination(movie)}>Nominate</Button> : <Button size="small" onClick={e => addNomination(movie)}>Nominate</Button>} </CardActions></CardContent></Card>)}
-          </Card>
+          <div className="search_results">
+            {movies.map(movie => <div><div><Typography>{movie.Title}</Typography><Typography>{movie.Year}</Typography>
+          <div>{nominations.includes(movie) ? <Button disabled size="small" onClick={e => addNomination(movie)}>Nominate</Button> : <Button size="small" onClick={e => addNomination(movie)}>Nominate</Button>} </div></div></div>)}
+          </div>
         :
         null}
        
         {<div> {nominations.length > 0
           ? 
-          nominations.map(nomination =>  <Card className={classes.nomination}>
-            <CardContent>
+          nominations.map(nomination =>  <div className="nomination">
+            <div>
           <Typography className={classes.title} color="textSecondary" gutterBottom> {nomination.Title}</Typography> 
           <Typography variant="h5" component="h2"> {nomination.Year}</Typography>
           <Typography className={classes.pos} color="textSecondary"> {nomination.imdbRating}</Typography>
           <Typography variant="body2" component="p">
        
           <br />
-          <CardActions><Button size="small" onClick={e => removeNomination(nomination)}>Remove Nomination</Button> </CardActions>
+          <div><Button size="small" onClick={e => removeNomination(nomination)}>Remove Nomination</Button> </div>
           </Typography>
-          </CardContent>
+          </div>
          
-          </Card>)
+          </div>)
          
           : null} 
           </div >}
